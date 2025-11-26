c93d2e5279e3a349717f4ef17731e055c508e639
Hub-bot24 <jdhugoaus@gmail.com>
2025-11-26 15:48:26 +1000

    Refactor service worker install and fetch events
    Removed caching logic and added skipWaiting during install.

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,5 @@
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('lot-robot').then(cache => {
-      return cache.addAll([
-        'index.html',
-        'styles.css',
-        'app.js',
-        'manifest.json',
-        'icon.png'
-      ]);
-    })
-  );
+self.addEventListener("install", e => {
+    self.skipWaiting();
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(resp => resp || fetch(e.request))
-  );
-});
+self.addEventListener("fetch", () => {});
