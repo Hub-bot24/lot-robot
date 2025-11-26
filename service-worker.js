2f4f25cb224f5f47600435ee2d037e2eaeff9260
Hub-bot24 <jdhugoaus@gmail.com>
2025-11-26 15:34:00 +1000

    Refactor service worker install and fetch events

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,19 @@
-self.addEventListener("install", e => {
-    e.waitUntil(
-        caches.open("lot-robot-cache").then(cache => {
-            return cache.addAll([
-                "index.html",
-                "styles.css",
-                "app.js",
-                "icon.png",
-                "manifest.json"
-            ]);
-        })
-    );
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('lot-robot').then(cache => {
+      return cache.addAll([
+        'index.html',
+        'styles.css',
+        'app.js',
+        'manifest.json',
+        'icon.png'
+      ]);
+    })
+  );
 });
 
-self.addEventListener("fetch", e => {
-    e.respondWith(
-        caches.match(e.request).then(response => {
-            return response || fetch(e.request);
-        })
-    );
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(resp => resp || fetch(e.request))
+  );
 });
