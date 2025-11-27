187a68e07526273588694f78a3c4670ea7ce3913
Hub-bot24 <jdhugoaus@gmail.com>
2025-11-27 14:10:53 +1000

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,9 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("lot-robot-cache").then(cache => {
-      return cache.addAll(["/", "index.html", "styles.css", "app.js", "manifest.json", "icon.png"]);
-    })
-  );
-});
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
+self.addEventListener("install", e=>{
+ e.waitUntil(caches.open("mlr").then(c=>c.addAll([
+ "index.html","manifest.json","icon.png"
+ ])));
+});
+self.addEventListener("fetch", e=>{
+ e.respondWith(caches.match(e.request).then(r=>r || fetch(e.request)));
 });
